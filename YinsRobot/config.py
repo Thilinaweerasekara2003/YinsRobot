d084a2aa91b52ff74ce8b0f0cfb18a8118c516fe
ThilinaWeerasekara <84437905+Thilinaweerasekara2003@users.noreply.github.com>
2022-07-08 10:50:31 +0530

    Update config.py

--- YinsRobot/config.py
+++ YinsRobot/config.py
@@ -17,14 +17,14 @@ class Config(object):
     API_ID = 123456  # integer value, dont use ""
     API_HASH = "awoo"
     TOKEN = "BOT_TOKEN"  # This var used to be API_KEY but it is now TOKEN, adjust accordingly.
-    OWNER_ID = 1606221784  # If you dont know, run the bot and do /id in your private chat with it, also an integer
-    OWNER_USERNAME = "AyiinXd"
-    SUPPORT_CHAT = "AyiinXdSupport"  # Your own group for support, do not add the @
+    OWNER_ID = 1377217980  # If you dont know, run the bot and do /id in your private chat with it, also an integer
+    OWNER_USERNAME = "Darkrider2003"
+    SUPPORT_CHAT = "Hitechrockets"  # Your own group for support, do not add the @
     JOIN_LOGGER = (
-        -1001432609692
+        -1001538122431
     )  # Prints any new group the bot is added to, prints just the name and ID.
     EVENT_LOGS = (
-        -1001150905176
+        -1001741346039
     )  # Prints information like gbans, sudo promotes, AI enabled disable states that may help in debugging and shit
 
     # RECOMMENDED
